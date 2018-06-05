d1834efee6b20223bd61811dbc75579c0e37e403
Thomas Dekiere <t.dekiere@gmail.com>
2018-06-05 23:35:14 +0200

    add an mdl layout for navigation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -165,7 +165,7 @@ function createIdeaItem(idea: IIdea): HTMLElement {
         </div>
         <div class="mdl-card__menu">
             <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                <i class="material-icons">share</i>
+                <i style="display:none;" class="material-icons">share</i>
             </button>
         </div>
     </div>
